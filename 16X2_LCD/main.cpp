506196256ebc00c8c16879bc2e321725b31f1841
MCLEANS <jackagwaya@gmail.com>
2020-02-02 16:05:35 +0300

    BASIC 16X2 LCD PROGRAM

---
+++ 16X2_LCD/main.cpp
@@ -0,0 +1,13 @@
+#include "mbed.h"
+#include "TextLCD.h"
+
+TextLCD lcd(PD_2,PD_3,PD_4,PD_5,PD_6,PD_7);
+
+int main(){
+    lcd.printf("hello world \n");
+    while(1){
+        
+        
+        }
+    
+    }
