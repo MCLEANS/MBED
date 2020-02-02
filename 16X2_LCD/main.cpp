69b6125c2f79eb323817b6daf1e31ced909dc099
MCLEANS <jackagwaya@gmail.com>
2020-02-02 16:39:41 +0300

    LCD button mased menu

--- 16X2_LCD/main.cpp
+++ 16X2_LCD/main.cpp
@@ -3,11 +3,78 @@
 
 TextLCD lcd(PD_2,PD_3,PD_4,PD_5,PD_6,PD_7);
 
+DigitalIn button1(PE_3,PullUp);
+DigitalIn button0(PE_4,PullUp);
+
+enum State{
+    FIRST,
+    SECOND,
+    THIRD
+};
+
 int main(){
-    lcd.printf("hello world \n");
+
+    char first_item[] = "1. Mango";
+    char second_item[] = "2. Orange";
+    char third_item[] = "3. Banana";
+    char fourth_item[] = "4. Apple";
+
+
+   int button_press = 1;
+   State menu_state = FIRST;
+
     while(1){
+
+        lcd.cls();
+
+        if(!button1){
+            button_press ++;
+            wait(0.1);
+        }
+        if(!button0){
+            button_press--;
+            wait(0.1);
+        }
+
+        if(button_press > 3){
+            button_press = 3;
+        }
+        if(button_press < 0){
+            button_press = 0;
+        }
+
+        if(button_press == 1){
+            menu_state = FIRST;
+        }
+        if (button_press == 2){
+           menu_state = SECOND;
+        }
+        if(button_press == 3){
+            menu_state = THIRD;
+        }
+
+        if(menu_state == FIRST){
+            lcd.locate(0,0);
+            lcd.printf(first_item);
+            lcd.locate(0,1);
+            lcd.printf(second_item);
+        }
         
+        if(menu_state == SECOND){
+            lcd.locate(0,0);
+            lcd.printf(second_item);
+            lcd.locate(0,1);
+            lcd.printf(third_item);
+        }
         
+        if(menu_state == THIRD){
+            lcd.locate(0,0);
+            lcd.printf(third_item);
+            lcd.locate(0,1);
+            lcd.printf(fourth_item);
+        }
+
+        wait(0.3);
         }
     
     }
